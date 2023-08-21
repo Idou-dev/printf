57d8f9abe25b656d0f204ffd17a9d80d0763b770
idou-dev <idouyoussef87@gmail.com>
2023-08-21 15:18:17 -0700

    returns the length of a string

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string
+ * Return: the length of a string
+ */
+
+int _strlen(char *str)
+{
+	int l = 0;
+
+	while(str[l] != '\0')
+		l++;
+	return (l);
+}
