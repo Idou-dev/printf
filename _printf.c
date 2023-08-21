d0bab8bf4eac9f141f4c9d3720481a501252ba69
idou-dev <idouyoussef87@gmail.com>
2023-08-21 06:22:31 -0700

    change files name

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: format type
+ * Return: the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list p;
+	int i = 0, n = 0;
+	char *s;
+
+	va_start(p, format);
+	if (!formt[i] || !format)
+		return (-1);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					putchar(va_arg(p, int));
+					n++;
+					break;
+				case 's':
+					s = va_arg(p, char *);
+					printf("%s", s);
+					n++;
+					break;
+				case '%':
+					putchar('%');
+					break;
+				default:
+					putchar(format[i]);
+					n++;
+					break;
+			}
+		}
+		else
+		{
+			putchar(format[i]);
+			n++;
+		}
+		i++;
+	}
+	va_end(p);
+	return (n);
+}
