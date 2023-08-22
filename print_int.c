4bc7d235d0c35f0a6ec9f84ab14899b2459a379d
Ahdyou <yousfiamal705@gmail.com>
2023-08-22 16:59:20 -0700

    intfonction

--- print_int.c
+++ print_int.c
@@ -8,15 +8,4 @@
  *
  * Return:number of chars printed
  */
-int print_int (va_list p, params_t *params)
- {
-    long l;
-        
-    if (params-> l_modifier)
-       l = va_arg(p,int)
-    else if (params-> h_modifier)
-       l = (short int) va_arg (p, int);
-    else 
-       l = (int) va_arg(p,int);
-    return(print_number(convert(l, 10, 0, params), params));
- }
+int print_int }
