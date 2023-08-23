93b75eb357ba8480aa60171fdd1c7081e94614fa
Ahdyou <125517508+Ahdyou@users.noreply.github.com>
2023-08-23 01:10:36 +0100

    Update print_int.c

--- print_int.c
+++ print_int.c
@@ -1,11 +1,1 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/**
- * print_int -print integer
- * @p:argument pointer
- *@params: the parameters struct
- *
- * Return:number of chars printed
- */
-int print_int }
